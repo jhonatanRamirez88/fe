d0ebb3910b5071fa49d568f2e0f9ba7eabe95246
jonass03 <jhonatan.ramirez@simtech.mx>
2017-05-01 22:16:07 -0500

    Initial commit

---
+++ src/main/java/com/ferreteria/FerreteriaApplication.java
@@ -0,0 +1,12 @@
+package com.ferreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaApplication.class, args);
+	}
+}

a46e239bd5ad50f3aa5969ebf6f06fad2dd9d7a3
jonass03 <jhonatan.ramirez@simtech.mx>
2017-05-04 21:01:43 -0500

    [CV] interceptor, constantes vistas

---
+++ src/main/java/com/ferreteria/constant/ViewConstant.java
@@ -0,0 +1,8 @@
+package com.ferreteria.constant;
+
+public class ViewConstant {
+	public static final String LOGIN = "login";
+	public static final String CONTACTS = "contacts";
+	public static final String CONTACT_FORM = "contactform";
+	
+}
